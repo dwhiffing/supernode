f2f64ef050785e2370d3b8fd6d21a91af476703c
Daniel Whiffing <danielwhiffing@gmail.com>
2019-04-12 16:28:25 -0400

    More refactoring

--- src/SearchView/components/SearchBar.js
+++ src/SearchView/components/SearchBar.js
@@ -1,20 +1,5 @@
 import React from 'react'
-import ResultsPreview from './ResultsPreview'
 import fuzzySearch from '../utils/fuzzySearch'
-
-export const SearchInput = ({ value, onChange, onSubmit }) => (
-  <input
-    type="text"
-    placeholder="Type a code snippet or function"
-    value={value}
-    onChange={onChange}
-    onKeyPress={e => {
-      if (e.key === 'Enter') {
-        onSubmit()
-      }
-    }}
-  />
-)
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -52,5 +37,42 @@ class SearchBar extends React.Component {
     this.props.onChange(query, fuzzySearch(query, this.props.indexedFunctions))
   }
 }
+
+const SearchInput = ({ value, onChange, onSubmit }) => (
+  <input
+    type="text"
+    placeholder="Type a code snippet or function"
+    value={value}
+    onChange={onChange}
+    onKeyPress={e => {
+      if (e.key === 'Enter') {
+        onSubmit()
+      }
+    }}
+  />
+)
+
+const ResultsPreview = ({ results, onClickResult }) =>
+  results.length > 0 && (
+    <div className="search-preview-results border" style={{ top: 25 }}>
+      {results.map((result, index) => (
+        <div
+          key={`result=${index}`}
+          className="search-preview-result"
+          onClick={() => onClickResult(index)}>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `<p>${result.highlightedName}</p>`,
+            }}
+          />
+        </div>
+      ))}
+      <p
+        onClick={() => onClickResult(0)}
+        style={{ marginTop: 30, marginBottom: 30 }}>
+        Press enter to see all results
+      </p>
+    </div>
+  )
 
 export default SearchBar
